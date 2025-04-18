797de97e4269feada9e38c71711b80a7af67c72b
Adam Czerepinski <aczerepinski@gmail.com>
2025-04-18 18:39:09 -0500

    replace custom form and handler with kit.com embed

--- src/web/email_list_controller.go
+++ src/web/email_list_controller.go
@@ -2,18 +2,12 @@ package web
 
 import (
 	"net/http"
-
-	email_list "github.com/aczerepinski/adamcz/src/email-list"
 )
 
 type emailListFormData struct {
-	EmailAddress string
-	FullName     string
-	Success      bool
-	Error        string
-	CDN          string
-	Version      string
-	MetaTitle    string
+	CDN       string
+	Version   string
+	MetaTitle string
 }
 
 // RenderSubscriptionForm renders the signup form (GET)
@@ -22,26 +16,6 @@ func (c *Controller) RenderSubscriptionForm(w http.ResponseWriter, r *http.Reque
 		CDN:       cdnHost,
 		Version:   c.version,
 		MetaTitle: "adamcz | subscribe",
-	}
-	c.templates["emailList"].Execute(w, data)
-}
-
-// ProcessSubscription handles form submission (POST)
-func (c *Controller) ProcessSubscription(w http.ResponseWriter, r *http.Request) {
-	data := emailListFormData{
-		CDN:       cdnHost,
-		Version:   c.version,
-		MetaTitle: "adamcz | subscribe",
-	}
-	email := r.FormValue("email")
-	data.EmailAddress = email
-	if email == "" {
-		data.Error = "Email address is required."
-	} else {
-		// Here you could add logic to save the signup, e.g. to a database or file
-		_ = email_list.Signup{EmailAddress: email}
-		data.Success = true
-		data.EmailAddress = ""
 	}
 	c.templates["emailList"].Execute(w, data)
 }
--- src/web/main_controller.go
+++ src/web/main_controller.go
@@ -62,8 +62,6 @@ func (c *Controller) emailListRouter(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodGet:
 			c.RenderSubscriptionForm(w, r)
-		case http.MethodPost:
-			c.ProcessSubscription(w, r)
 		default:
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		}
